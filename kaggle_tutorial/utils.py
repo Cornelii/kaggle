39d2e8f3b6ff9b3e77f4bc83eebb929ca8227e0c
unknown <onelife0985@gmail.com>
2019-03-16 02:51:28 +0900

    finish kaggle-tutorial

--- kaggle_tutorial/utils.py
+++ kaggle_tutorial/utils.py
@@ -18,8 +18,6 @@ def preprocessing(df_train):
     df_train['avg_room'] = df_train.sqft_above / (df_train.rooms+1)
     df_train['sqft_inner_total'] = df_train.sqft_above+df_train.sqft_basement
     df_train['sqft_total'] = df_train.sqft_inner_total + df_train.sqft_lot
-    df_train['sqft_total_origin'] = df_train.sqft_living + df_train.sqft_lot
-    df_train['sqft_total_15'] = df_train.sqft_living15 + df_train.sqft_lot15
     
     
     lambda_fn = lambda row: row['year'] - row['yr_built'] + 2
@@ -28,12 +26,12 @@ def preprocessing(df_train):
     df_train['lat'] = df_train['lat'].values - 47
     df_train['long'] = df_train['long'].values + 123
     
-#     zip_index = df_train.loc[df_train['price']>=2000000].groupby('zipcode').sum().index
-#     df_train['mask_zipcode'] = df_train['zipcode'].apply(lambda x: 1 if x in zip_index else 0)
+    zip_index = df_train.loc[df_train['waterfront']==1].groupby('zipcode').sum().index
+    df_train['mask_zipcode'] = df_train['zipcode'].apply(lambda x: 1 if x in zip_index else 0)
     
     skew_columns = ['rooms','sqft_living', 'sqft_lot', 'sqft_above', 'sqft_basement',
                     'lat','long','sqft_living15','sqft_lot15','sqft_inner_total',
-                    'sqft_total','sqft_total_origin','sqft_total_15','age']
+                    'sqft_total','age']
 
     for col in skew_columns:
         df_train[col] = df_train[col].apply(apply_log)
